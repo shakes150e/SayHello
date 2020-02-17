af7ba3d908eaf6d5e1d043fc78173acc79ebca58
shake <shakes150e@gmail.com>
2020-02-17 18:50:18 +0200

    initial commit

---
+++ src/org/launchcode/Main.java
@@ -0,0 +1,10 @@
+package org.launchcode;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Shakes150e !!");
+        System.out.println("Crazy Code WoW........dead");
+    }
+}
